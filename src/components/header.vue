e1ab24e564ab5549b3a48abd3840f7b463a5a042
emazyka <emazyka@users.noreply.github.com>
2021-12-09 08:33:08 +0000

    deploy: 6eba6fe68d3f24e3999d0db097958597013715ec

--- src/components/header.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="header">
-    <img src="./../assets/logo.png" alt="logo" class="logo">
-  </div>
-  <div class="pagetitle">
-    <h1>VueJS Athens Meetup</h1>
-  </div>
-</template>
